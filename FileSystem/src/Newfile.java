5896d46c54f7830926d13cae593eca425eb3938c
kau14247 <kau14247@sheridancollege.ca>
2023-06-01 17:22:08 -0400

    Student detail version 2

---
+++ FileSystem/src/Newfile.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Ravee
+ */
+public class Newfile {
+     private String suit; //clubs, spades, diamonds, hearts
+   private int value;//1-13
+
+   public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
+    /**
+     * @return the suit
+     */
+    public String getSuit() {
+        return suit;
+    }
+
+    /**
+     * @param suit the suit to set
+     */
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    /**
+     * @return the value
+     */
+    public int getValue() {
+        return value;
+    }
+
+    /**
+     * @param value the value to set
+     */
+    public void setValue(int value) {
+        this.value = value;
+    }
+    
+    public Card(String suit, int Value){
+        this.suit = suit;
+        this.value= Value;
+    }
+    public String toString(){
+        String cardValue=" ";
+        
+        switch(value) {
+            case 1:
+              cardValue = "Ace";
+              break;
+            case 11:
+              cardValue ="Jack";
+              break;
+            case 12:
+              cardValue="Queen";
+              break;
+             
+            case 13:
+              cardValue="King";
+              break;
+        } 
+    return cardValue +" "+ suit;
+             
+              
+    }
+   
+    
+    
+}
